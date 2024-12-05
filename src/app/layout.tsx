dfe9d7d53ef83717f246c8b64223925f3e1bf47f
egokiemute <ego.okiemute@gmail.com>
2024-12-06 00:37:41 +0100

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from "next";
 import { Inter, Calistoga } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 
-const inter = Inter({ subsets: ['latin'], variable: "--font-sans"});
-const calistoga = Calistoga({ subsets: ['latin'], variable: "--font-serif", weight: ["400"]});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "A software developer",
-  description: "Crafting websites that are beautiful, compelling & performant for disruptive startups and creators.",
+  description:
+    "Crafting websites that are beautiful, compelling & performant for disruptive startups and creators.",
   icons: {
     icon: "/favicon.ico",
   },
@@ -20,8 +26,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={twMerge(inter.variable, calistoga.variable, "bg-white/30 text-gray-900 antialiased font-sans")}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body
+          className={twMerge(
+            inter.variable,
+            calistoga.variable,
+            "bg-white/30 text-gray-900 antialiased font-sans"
+          )}
+        >
+          {children}
+          <Analytics />
+        </body>
+      </html>
+    </>
   );
 }
